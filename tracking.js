2eaae6a7f4bb4ea8a883577833f70a8e5acf67e7
Cu <xlin6328@gmail.com>
2026-06-20 10:08:08 +0800

    Update tracking.js

--- tracking.js
+++ tracking.js
@@ -22,3 +22,55 @@ document.getElementById("order-id").innerText =
 
 const status = order.status || 1;
 
+document.getElementById("customer-info")
+.innerHTML = `
+
+<p>
+收件人：
+${order.name}
+</p>
+
+<p>
+電話：
+${order.phone}
+</p>
+
+<p>
+超商：
+${order.storeType}
+</p>
+
+<p>
+門市：
+${order.storeName}
+</p>
+
+<p>
+總金額：
+$${order.total}
+</p>
+
+`;
+
+let html = "<h3>購買商品</h3>";
+
+order.items.forEach(item => {
+
+    html += `
+
+    <p>
+
+    ${item.name}
+
+    ×
+
+    ${item.qty}
+
+    </p>
+
+    `;
+
+});
+
+document.getElementById("order-items")
+.innerHTML = html;
