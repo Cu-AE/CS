dea23d94a165fc29e4ce94e0707c20773a2c0ee0
Cu <xlin6328@gmail.com>
2026-07-02 11:38:00 +0800

    Create products.js

---
+++ js/products.js
@@ -0,0 +1,159 @@
+// 商品資料（多加 hot 屬性）
+const products = [
+  { 
+    name:"aespa THE HBD PARTY HOUSE FOR WINTER",
+    images:[
+        "images/aespa-2025-HBD-Winter.jpg",
+        "images/aespa-2025-HBD-Winter.jpg",
+        "images/aespa-2025-HBD-Winter.jpg",
+        "images/aespa-2025-HBD-Winter.jpg"
+        ],
+    img:"images/aespa-2025-HBD-Winter.jpg",
+    price:350,
+    category:"aespa",
+    badge:"HOT",
+    badgeColor:"red",
+    desc:"aespa 官方限定商品",
+    rating:4.8
+  },
+  { 
+    name:"Rich Man ",
+    price:299,
+    category:"aespa",
+    img:"https://picsum.photos/220?0",
+    badge:"NEW",
+    badgeColor:"red",
+    desc:"aespa 最新專輯周邊商品",
+    rating:5
+  },
+  { 
+    name:"dirty work2",
+    price:350,
+    category:"aespa",
+    img:"https://picsum.photos/220?2",
+    badge:"SALE",
+    badgeColor:"green",
+    desc:"aespa 最新專輯周邊商品",
+    rating:4.9
+  },
+  { 
+    name:"Blue Valentine",
+    price:250,
+    category:"nmixx",
+    img:"https://picsum.photos/220?3",
+    badge:"-20%",
+    badgeColor:"red",
+    desc:"nmixx 最新專輯周邊商品",
+    rating:3.8 
+  },
+  {
+    name:"Blue 2",
+    price:320,
+    category:"nmixx",
+    img:"https://picsum.photos/220?4",
+    badge:"-20%",
+    badgeColor:"red",
+    desc:"nmixx 最新專輯周邊商品",
+    rating:4.8
+  },
+  { 
+    name:"xoxz",
+    price:120,
+    category:"ive",
+    img:"https://picsum.photos/220?5",
+    badge:"HOT",
+    badgeColor:"red",
+    desc:"ive 最新專輯周邊商品",
+    rating:4.8
+  },
+  { 
+    name:"I am",
+    price:80,
+    category:"ive",
+    img: "https://picsum.photos/220?6",
+    badge:"HOT",
+    badgeColor:"red",
+    desc: "ive 最新專輯周邊商品" ,
+    rating:4.8
+  },
+  { 
+    name: "jump",
+    price: 120,
+    category: "Blackpink",
+    img: "https://picsum.photos/220?7",
+    badge:"HOT",
+    badgeColor:"red",
+    desc: "Blackpink 最新專輯周邊商品",
+    rating:4.8
+  },
+  { 
+    name: "3am",
+    price: 120, 
+    category: "Rose", 
+    img: "https://picsum.photos/220?8", 
+    badge:"HOT", 
+    badgeColor:"red",
+    desc: "Rose 最新專輯周邊商品",
+    rating:4.8 
+  },
+  { 
+    name: "we are", 
+    price: 120, 
+    category: "idle", 
+    img: "https://picsum.photos/220?9", 
+    badge:"HOT", 
+    badgeColor:"red",
+    desc: "idle 最新專輯周邊商品" ,
+    rating:4.8 
+  },
+  { 
+    name: "babymonster", 
+    price: 120, 
+    category: "babymonster", 
+    img: "https://picsum.photos/220?10", 
+    badge:"HOT", 
+    badgeColor:"red",
+    desc: "babymonster 最新專輯周邊商品",
+    rating:4.8 
+  },
+  { 
+    name: "1", 
+    price: 1200, 
+    category: "1", 
+    img: "https://picsum.photos/220?11", 
+    badge:"HOT", 
+    badgeColor:"red",
+    desc: "1 最新專輯周邊商品" ,
+    rating:4.8 
+  },
+  { 
+    name: "2", 
+    price: 1300, 
+    category: "2", 
+    img: "https://picsum.photos/220?12", 
+    badge:"HOT", 
+    badgeColor:"red",
+    desc: "2 最新專輯周邊商品" ,
+    rating:4.8 
+  },
+  { 
+    name: "3", 
+    price: 1400, 
+    category: "3", 
+    img: "https://picsum.photos/220?13", 
+    badge:"HOT", 
+    badgeColor:"red",
+    desc: "3 最新專輯周邊商品" ,
+    rating:4.8 
+  },
+  { 
+    name: "4", 
+    price: 1500, 
+    category: "4", 
+    img: "https://picsum.photos/220?14", 
+    badge:"HOT", 
+    badgeColor:"red",
+    desc: "4 最新專輯周邊商品" ,
+    rating:4.8 
+  }
+];
